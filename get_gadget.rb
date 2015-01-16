cd070ba1deb26225b740f557022125c4f7d5d82e
OpenShift System User <54b7a25ce0b8cd1c18000097@ex-std-node142.prod.rhcloud.com>
2015-01-16 02:52:30 -0500

    Modify get_gadget.rb not to display (bad) instructions

--- get_gadget.rb
+++ get_gadget.rb
@@ -16,18 +16,23 @@ begin
 
   i.each do |i_elem|
     for j in 1..4 do 
-      cmd = "objdump -M intel -D -b binary -m i386 #{filename} --start-address=%d --stop-address=%d| grep '^ '"%([(i_elem-j-1), (i_elem+2)])
+      cmd = "objdump -M intel -D -b binary -m i386 #{filename} --start-address=%d --stop-address=%d| grep '^ '"%([(i_elem-j-1), (i_elem+1)])
       Open3.popen3(cmd) do |stdin, stdout, stderr|
         stdout.each do |line|
+          if line.empty? || line.index('(bad)') || line.index('<internal disassembler error>') then
+            next
+          end
           print line
-          break
+          #break
         end
-        break
+        print "\n"
+      #  break
       end
     end
   end
 
 rescue Exception => e
+  pp e.message
   pp e.backtrace
   exit 1
 end
